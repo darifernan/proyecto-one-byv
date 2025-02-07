5068a3f9434e8dcc0506c7504b5b4ce449540b4d
darifernan <barriosyvecinos@gmail.com>
2025-02-07 06:05:54 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log("ejecutando el sistema")
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
